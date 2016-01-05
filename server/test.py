1de087a215f3bc98760d85df3d636d48e5263ac6
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-01-05 15:10:44 -0500

    fix(test): Fix server test suite

--- server/test.py
+++ server/test.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
 import server
 import unittest
+import os
+from inspect import getmembers
 
 
 class ReadModFromUrl(unittest.TestCase):
 
     def test_find_mod_for_url_html_default(self):
         url = 'https://www.google.com'
-        server.setup()
+        server.setup('dev')
 
         handler = server.find_mod_for_url(url)
+        handler_mod_name  = getmembers(handler)[0][1].__module__
 
-        self.assertEqual("handlers.html_default", handler.__name__)
+        self.assertEqual("handlers.html_default", handler_mod_name)
 
     def test_find_mod_for_url_youtube(self):
         url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
         server.setup()
 
         handler = server.find_mod_for_url(url)
+        handler_mod_name  = getmembers(handler)[0][1].__module__
 
-        self.assertEqual("handlers.youtube", handler.__name__)
+        self.assertEqual("handlers.youtube", handler_mod_name)
 
     def test_find_mod_for_url_youtube_with_other_params(self):
         url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=xxxxx'
         server.setup()
 
         handler = server.find_mod_for_url(url)
+        handler_mod_name  = getmembers(handler)[0][1].__module__
 
-        self.assertEqual("handlers.youtube", handler.__name__)
+        self.assertEqual("handlers.youtube", handler_mod_name)
 
 if __name__ == "__main__":
     unittest.main()
